e44c8f9799d49e42a205e8dda2dae08f7e17f0b4
NitinV2000 <nitin.vankadari2018@vitstudent.ac.in>
2022-05-28 16:03:55 +0530

    Added h2 db code also

--- src/main/java/com/tintin/springjwt/SpringBootSecurityJwtApplication.java
+++ src/main/java/com/tintin/springjwt/SpringBootSecurityJwtApplication.java
@@ -1,7 +1,15 @@
 package com.tintin.springjwt;
 
+import java.util.ArrayList;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.tintin.springjwt.model.ERole;
+import com.tintin.springjwt.model.Role;
+import com.tintin.springjwt.repository.RoleRepository;
 
 @SpringBootApplication
 public class SpringBootSecurityJwtApplication {
@@ -9,5 +17,14 @@ public class SpringBootSecurityJwtApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
 	}
-
+	
+	@Bean
+	CommandLineRunner run(RoleRepository roleRepository) {
+		return args -> {
+			
+			roleRepository.save(new Role(ERole.ROLE_ADMIN));
+			roleRepository.save(new Role(ERole.ROLE_USER));
+			roleRepository.save(new Role(ERole.ROLE_MODERATOR));
+		};
+	}
 }
--- src/main/java/com/tintin/springjwt/security/WebSecurityConfig.java
+++ src/main/java/com/tintin/springjwt/security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -36,6 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new AuthTokenFilter();
 	}
 
+
 	@Override
 	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
 		authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
@@ -54,6 +56,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+//		http.cors().and().csrf().ignoringAntMatchers("/h2-console/**").and().cors().disable()
+//		http.authorizeRequests().antMatchers("/h2-console/**").permitAll()
+//        .and().csrf().ignoringAntMatchers("/h2-console/**")
+//        .and().headers().frameOptions().sameOrigin().and()
 		http.cors().and().csrf().disable()
 			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
